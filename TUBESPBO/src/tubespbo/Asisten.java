8a179f1a9c51314e21f743834254f29becafb089
rashiefm <rashiefm@gmail.com>
2016-03-11 16:02:45 +0700

    inii

--- TUBESPBO/src/tubespbo/Asisten.java
+++ TUBESPBO/src/tubespbo/Asisten.java
@@ -47,5 +47,6 @@ public class Asisten {
 	}
 	public int getIndex(){
 		return index;
-	}
+	
+        }
 }
--- TUBESPBO/src/tubespbo/Orang.java
+++ TUBESPBO/src/tubespbo/Orang.java
@@ -45,5 +45,6 @@ public class Orang {
 	}
 	public String getStatus(){
 		return status;
-	}
+	
+        }
 }
